c7de932ad7fd1dccc58257853297fc2ba0808989
David-Moreira <david_nogueira_moreira@hotmail.com>
2020-09-19 15:12:31 +0100

    Initial commit | Ongoing implementation

---
+++ ExpressionDecomposer/Interfaces/IExpressionDecomposer.cs
@@ -0,0 +1,10 @@
+﻿using ExpressionDecomposer.Models;
+using System.Linq.Expressions;
+
+namespace ExpressionDecomposer.Interface
+{
+    public interface IExpressionDecomposer
+    {
+        ExpressionDecomposed DecomposeExpression(Expression expression);
+    }
+}
---
+++ ExpressionDecomposer/Models/ExpressionDecomposed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ExpressionDecomposer.Models
+{
+    public class ExpressionDecomposed
+    {
+        public IEnumerable<string> OrderedPropertyNames { get; set; }
+        public string PropertyName { get; set; }
+        public string Operator { get; set; }
+
+        /// <summary>
+        /// Retrieves the accessor path
+        /// </summary>
+        /// <returns></returns>
+        public string GetPath()
+        {
+            return string.Join(".", OrderedPropertyNames);
+        }
+    }
+}
